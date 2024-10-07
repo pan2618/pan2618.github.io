83552336c76323f71936543e1b065a022d1b5b9b
pan2618 <165255144+pan2618@users.noreply.github.com>
2024-10-08 01:59:01 +0800

    Update slideshow.js

--- Shuttlecock web/slideshow.js
+++ Shuttlecock web/slideshow.js
@@ -56,37 +56,3 @@ function showSlides() {
 showSlides();
 
 
-document.addEventListener('DOMContentLoaded', function() {
-    const paragraphs = document.querySelectorAll('.fade-in');
-
-    paragraphs.forEach((p, index) => {
-        setTimeout(() => {
-            p.classList.add('show');
-            smoothScrollTo(p, 6000);  // 設定滾動時間 1000 毫秒 (1 秒)
-        }, index * 6000);  // 每行延遲1.1秒
-    });
-
-    function smoothScrollTo(element, duration) {
-        const targetPosition = element.getBoundingClientRect().top + window.pageYOffset;
-        const startPosition = window.pageYOffset;
-        const distance = targetPosition - startPosition;
-        let startTime = null;
-
-        function animation(currentTime) {
-            if (startTime === null) startTime = currentTime;
-            const timeElapsed = currentTime - startTime;
-            const run = ease(timeElapsed, startPosition, distance, duration);
-            window.scrollTo(0, run);
-            if (timeElapsed < duration) requestAnimationFrame(animation);
-        }
-
-        function ease(t, b, c, d) {
-            t /= d / 2;
-            if (t < 1) return c / 2 * t * t + b;
-            t--;
-            return -c / 2 * (t * (t - 2) - 1) + b;
-        }
-
-        requestAnimationFrame(animation);
-    }
-});
